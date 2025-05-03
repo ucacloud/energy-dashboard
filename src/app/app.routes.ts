772c2f1fb6857d3640680128942ef02e25237264
ucacloud <mgcnlrar@gmail.com>
2025-05-03 17:06:10 -0500

    created LayoutComponent "frame"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
