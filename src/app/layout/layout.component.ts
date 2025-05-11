dfbdd46063741557a442ef445264abcdd4bd4bcc
ucacloud <mgcnlrar@gmail.com>
2025-05-11 16:55:21 -0500

    added RouterModule to layout.component to fix links

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, RouterModule, CommonModule],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
